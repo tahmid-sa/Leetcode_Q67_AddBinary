72c6a6361a1d9177dc2d21fd6a5423773ee28bbf
Tahmid Sajin <tahmid.sajin.1999@gmail.com>
2022-06-17 22:10:19 -0400

    Adding files to github

---
+++ Q67_AddBinary/Q67_AddBinary.cpp
@@ -0,0 +1,93 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+string addBinary(string a, string b) {
+    int bitMax, carry = 0;
+    string result;
+
+    if (a.size() >= b.size()) {
+        bitMax = a.size();
+        string zeros = std::string(bitMax - b.size(), '0');
+        b.insert(0, zeros);
+    }
+    else if (a.size() < b.size()) {
+        bitMax = b.size();
+        string zeros = std::string(bitMax - a.size(), '0');
+        a.insert(0, zeros);
+    }
+
+    cout << a << endl;
+    cout << b << endl;
+
+    reverse(a.begin(), a.end());
+    reverse(b.begin(), b.end());
+
+    for (int i = 0; i < bitMax; i++) {
+        if (a[i] == '0' && b[i] == '0') {
+            if (carry == 1) {
+                result += '1';
+                carry = 0;
+            }
+            else if (carry == 0) {
+                result += '0';
+            }
+        }
+        else if (a[i] == '0' && b[i] == '1') {
+            if (carry == 1) {
+                result += '0';
+
+                if (i == bitMax - 1) {
+                    result += '1';
+                }
+            }
+            else if (carry == 0) {
+                result += '1';
+            }
+        }
+        else if (a[i] == '1' && b[i] == '0') {
+            if (carry == 1) {
+                result += '0';
+
+                if (i == bitMax - 1) {
+                    result += '1';
+                }
+            }
+            else if (carry == 0) {
+                result += '1';
+            }
+        }
+        else if (a[i] == '1' && b[i] == '1') {
+            if (carry == 1) {
+                result += '1';
+
+                if (i == bitMax - 1) {
+                    result += '1';
+                }
+            }
+            else if (carry == 0) {
+                result += '0';
+                carry = 1;
+
+                if (i == bitMax - 1) {
+                    result += '1';
+                }
+            }
+        }
+    }
+
+    reverse(result.begin(), result.end());
+
+    return result;
+}
+
+int main()
+{
+    string aBit = "1010"; //01011
+    string bBit = "1011";  //1101
+
+    cout << addBinary(aBit, bBit) << endl;
+
+    return 0;
+}
